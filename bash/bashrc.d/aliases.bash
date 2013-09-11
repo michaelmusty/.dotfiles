e33ac1fc5369902745fd660a5cf9f06034c6e4a6
Tom Ryder <tom@sanctum.geek.nz>
2013-09-11 13:17:54 +1200

    Add ''ll'' alias for ''ls -al''

--- bash/bashrc.d/aliases.bash
+++ bash/bashrc.d/aliases.bash
@@ -4,6 +4,9 @@ alias diff='diff -u'
 # Don't print the GDB copyright message on every invocation
 alias gdb='gdb -q'
 
+# Quick way to get ls -al, since I do that a lot
+alias ll='ls -al'
+
 # Refuse to do highly destructive things in the MySQL client; prevents me from
 # updating or deleting rows without a WHERE clause, among other things
 alias mysql='mysql --safe-updates'
