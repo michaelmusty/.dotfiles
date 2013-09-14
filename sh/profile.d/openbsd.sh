379776f6e3b349d7865f4280ff6bca90d359946e
Tom Ryder <tom@sanctum.geek.nz>
2013-09-15 01:46:09 +1200

    Degrade TERM for OpenBSD systems

---
+++ sh/profile.d/openbsd.sh
@@ -0,0 +1,9 @@
+# OpenBSD systems don't handle 256 color terminals the way my Linux systems do,
+# seeming to be inconsistent about e.g. the correct number of parameters for
+# `tput setaf ...` -- I don't know which of them is right but for the moment
+# I'm siding with Linux. This chops off any -256color suffix to the terminal
+# name.
+if [ "$(uname -s)" = "OpenBSD" ]; then
+    TERM=${TERM%-256color}
+fi
+
