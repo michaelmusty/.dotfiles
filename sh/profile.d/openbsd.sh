8d6fdde0629614d8e05751a695c1365c8555dc08
Tom Ryder <tom@sanctum.geek.nz>
2013-09-15 02:51:56 +1200

    Chop off unicode suffix for OpenBSD TERM too

--- sh/profile.d/openbsd.sh
+++ sh/profile.d/openbsd.sh
@@ -2,8 +2,9 @@
 # seeming to be inconsistent about e.g. the correct number of parameters for
 # `tput setaf ...` -- I don't know which of them is right but for the moment
 # I'm siding with Linux. This chops off any -256color suffix to the terminal
-# name.
+# name, and also any -unicode suffix.
 if [ "$(uname -s)" = "OpenBSD" ]; then
     TERM=${TERM%-256color}
+    TERM=${TERM%-unicode}
 fi
 
