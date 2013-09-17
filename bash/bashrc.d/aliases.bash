18cdad1090f86481740ec4f2a1a5171e09e0531f
Tom Ryder <tom@sanctum.geek.nz>
2013-09-17 13:08:40 +1200

    safe-updates now configured in ~/.my.cnf

--- bash/bashrc.d/aliases.bash
+++ bash/bashrc.d/aliases.bash
@@ -7,7 +7,3 @@ alias gdb='gdb -q'
 # Quick way to get ls -al, since I do that a lot
 alias ll='ls -al'
 
-# Refuse to do highly destructive things in the MySQL client; prevents me from
-# updating or deleting rows without a WHERE clause, among other things
-alias mysql='mysql --safe-updates'
-
