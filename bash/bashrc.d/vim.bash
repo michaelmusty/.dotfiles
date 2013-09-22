73521c268fed201b1d7db5a0d4495b8d2130c7f6
Tom Ryder <tom@sanctum.geek.nz>
2013-09-23 11:02:57 +1200

    Add more Vim aliases; still tinkering with BSDs

--- bash/bashrc.d/vim.bash
+++ bash/bashrc.d/vim.bash
@@ -1,9 +1,23 @@
-# If Vim exists on the system, use it instead of vi
+# If Vim exists on the system, use it instead of ex, vi, and view
+ex() {
+    if hash vim 2>/dev/null; then
+        command vim -e "$@"
+    else
+        command ex "$@"
+    fi
+}
 vi() {
     if hash vim 2>/dev/null; then
         command vim "$@"
     else
         command vi "$@"
+    fi
+}
+view() {
+    if hash vim 2>/dev/null; then
+        command vim -R "$@"
+    else
+        command view "$@"
     fi
 }
 
