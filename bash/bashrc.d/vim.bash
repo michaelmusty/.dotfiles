afe7f306f39f63c9ce2506df786d7b4281715733
Tom Ryder <tom@sanctum.geek.nz>
2013-10-23 11:26:15 +1300

    Replace Vim functions with simple aliases

--- bash/bashrc.d/vim.bash
+++ bash/bashrc.d/vim.bash
@@ -4,13 +4,7 @@ if ! hash vim 2>/dev/null; then
 fi
 
 # If Vim exists on the system, use it instead of ex, vi, and view
-ex() {
-    command vim -e "$@"
-}
-vi() {
-    command vim "$@"
-}
-view() {
-    command vim -R "$@"
-}
+alias ex='vim -e'
+alias vi='vim'
+alias view='vim -R'
 
