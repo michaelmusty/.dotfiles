440888a28a5088dbb9eb8c7015ec454bbdcacbef
Tom Ryder <tom@sanctum.geek.nz>
2013-12-11 15:55:08 +1300

    Little function to count files

---
+++ bash/bashrc.d/cf.bash
@@ -0,0 +1,25 @@
+# Count files
+cf() {
+    local dir dgs ngs
+    local -a files
+
+    # Record current state of dotglob and nullglob
+    shopt -pq dotglob && dgs=1
+    shopt -pq nullglob && ngs=1
+
+    # Specify directory to check
+    dir=${1:-$PWD}
+
+    # Retrieve the files array
+    shopt -s dotglob nullglob
+    files=("$dir"/*)
+
+    # Reset our options
+    ((dgs)) && shopt -s dotglob
+    ((ngs)) && shopt -s nullglob
+
+    # Print result
+    printf 'cf: %'\''.f files in %s\n' \
+        "${#files[@]}" "$dir"
+}
+
