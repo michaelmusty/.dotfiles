0f527bd7c2aa4a7b81ea6f29fe8ab0ed538b0859
Tom Ryder <tom@sanctum.geek.nz>
2013-12-11 16:06:51 +1300

    Error message if not a directory

--- bash/bashrc.d/cf.bash
+++ bash/bashrc.d/cf.bash
@@ -3,12 +3,17 @@ cf() {
     local dir dgs ngs
     local -a files
 
+    # Specify directory to check
+    dir=${1:-$PWD}
+    if [[ ! -d $dir ]]; then
+        printf 'bash: cf: %s is not a directory\n' \
+            "$dir" >&2
+        return 1
+    fi
+
     # Record current state of dotglob and nullglob
     shopt -pq dotglob && dgs=1
     shopt -pq nullglob && ngs=1
-
-    # Specify directory to check
-    dir=${1:-$PWD}
 
     # Retrieve the files array
     shopt -s dotglob nullglob
