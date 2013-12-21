61a3fe778aafa96aca859e844c2aa567285d5f55
Tom Ryder <tom@sanctum.geek.nz>
2013-12-21 22:20:18 +1300

    Change null-arguments check for tmux func

--- bash/bashrc.d/tmux.bash
+++ bash/bashrc.d/tmux.bash
@@ -7,8 +7,8 @@ fi
 tmux() {
 
     # If sessions exist, default the arguments to the attach-session command
-    if command tmux has-session 2>/dev/null; then
-        command tmux "${@:-attach-session}"
+    if ! (($#)) && command tmux has-session 2>/dev/null; then
+        command tmux attach-session
 
     # Otherwise, just call tmux directly with the given arguments
     else
