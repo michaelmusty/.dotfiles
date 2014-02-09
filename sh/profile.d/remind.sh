6d0818b35abafbf6c6c1500753570b2dce188317
Tom Ryder <tom@sanctum.geek.nz>
2014-02-10 10:41:09 +1300

    No longer need to strip newlines from reminders
    Turns out ending a reminder line with % prevents it adding a newline, so
    I don't need sed(1) to do that now

--- sh/profile.d/remind.sh
+++ sh/profile.d/remind.sh
@@ -1,6 +1,6 @@
 # Show reminders on login
 if command -v remind >/dev/null 2>&1 && [ -f "$HOME"/.reminders ] ; then
     printf '\n'
-    remind "$HOME"/.reminders | sed /^$/d
+    remind "$HOME"/.reminders
 fi
 
