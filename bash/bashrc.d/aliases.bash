03409fa0767b65422bc06f67214d88130d268cd0
Tom Ryder <tom@sanctum.geek.nz>
2014-02-20 20:53:42 +1300

    Don't print bc(1) welcome message

--- bash/bashrc.d/aliases.bash
+++ bash/bashrc.d/aliases.bash
@@ -1,6 +1,9 @@
 # Print a terminal bell
 alias bell='printf %s '$'\a'
 
+# Don't print the bc(1) welcome message
+alias bc='bc -q'
+
 # Use a unified format for diff by default
 alias diff='diff -u'
 
