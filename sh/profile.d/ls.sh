b8f32111199c4492395dd16804bba0991df9c524
Tom Ryder <tom@sanctum.geek.nz>
2014-10-10 18:39:23 +1300

    Add missing options terminator

--- sh/profile.d/ls.sh
+++ sh/profile.d/ls.sh
@@ -1,7 +1,7 @@
 # Define and store appropriate colors for ls
 if command -v dircolors >/dev/null 2>&1 ; then
     if [ -r "$HOME"/.dircolors ] ; then
-        eval "$(dircolors --sh "$HOME"/.dircolors)"
+        eval "$(dircolors --sh -- "$HOME"/.dircolors)"
     else
         eval "$(dircolors --sh)"
     fi
