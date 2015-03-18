38d5b7166a291bb7b20cd772cccc29301ac78ba3
Tom Ryder <tom@sanctum.geek.nz>
2015-03-19 12:45:15 +1300

    Allow supplying pattern string to scr

--- bash/bashrc.d/scr.bash
+++ bash/bashrc.d/scr.bash
@@ -1,6 +1,12 @@
 # Create a temporary directory and change into it, to stop me putting stray
-# files into $HOME, and making the system do cleanup for me
+# files into $HOME, and making the system do cleanup for me. Single optional
+# argument is the string to use for naming the directory; defaults to "scr".
 scr() {
-    pushd -- "$(mktemp -d)"
+    if (($# <= 1)) ; then
+        pushd -- "$(mktemp -dt "${1:-scr}".XXXXX)"
+    else
+        printf 'bash: scr: too many arguments\n' >&2
+        return 1
+    fi
 }
 
