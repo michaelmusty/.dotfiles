a0ca55ad53acffd2fa87ff84dfba7bccec92dfc6
Tom Ryder <tom@sanctum.geek.nz>
2015-06-04 11:58:03 +1200

    Support -p option to vared

--- bash/bashrc.d/vared.bash
+++ bash/bashrc.d/vared.bash
@@ -5,6 +5,21 @@ fi
 
 # Edit named variables' values
 vared() {
+    local opt prompt
+    local OPTERR OPTIND OPTARG
+    while getopts 'p:' ; do
+        case $opt in
+            p)
+                prompt=$OPTARG
+                ;;
+            \?)
+                printf 'bash: %s: -%s: invalid option\n'
+                    "$FUNCNAME" "$opt" >&2
+                return 2
+                ;;
+        esac
+    done
+    shift "$((OPTIND-1))"
     if ! (($#)) ; then
         printf 'bash: %s: No variable names given\n' \
             "$FUNCNAME" >&2
@@ -12,7 +27,7 @@ vared() {
     fi
     local name
     for name in "$@" ; do
-        IFS= read -e -i "${!name}" -p "$name"= -r "$name"
+        IFS= read -e -i "${!name}" -p "${prompt:-$name=}" -r -- "$name"
     done
 }
 complete -A variable vared
