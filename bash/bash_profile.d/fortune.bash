7ec37c45811020c06bc91f5a95361f6f0fce1f66
Tom Ryder <tom@sanctum.geek.nz>
2015-06-10 10:46:54 +1200

    Limit to slightly shorter fortunes

--- bash/bash_profile.d/fortune.bash
+++ bash/bash_profile.d/fortune.bash
@@ -3,6 +3,6 @@ if [[ $- == *i* ]] && hash fortune 2>/dev/null ; then (
     if [[ -d "$HOME"/.local/share/games/fortunes ]] ; then
         FORTUNE_PATH=${FORTUNE_PATH:-$HOME/.local/share/games/fortunes}
     fi
-    fortune -sn "${FORTUNE_MAXSIZE:-1024}" "$FORTUNE_PATH"
+    fortune -sn "${FORTUNE_MAXSIZE:-768}" "$FORTUNE_PATH"
 ) ; fi
 
