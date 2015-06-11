b83fe7d6b69cb0a44a06778a90afa2a70525946f
Tom Ryder <tom@sanctum.geek.nz>
2015-06-11 21:21:50 +1200

    Move first newline back into fortune

--- bash/bash_profile.d/fortune.bash
+++ bash/bash_profile.d/fortune.bash
@@ -3,6 +3,7 @@ if [[ $- == *i* ]] && hash fortune 2>/dev/null ; then (
     if [[ -d "$HOME"/.local/share/games/fortunes ]] ; then
         FORTUNE_PATH=${FORTUNE_PATH:-$HOME/.local/share/games/fortunes}
     fi
+    printf '\n'
     fortune -sn "${FORTUNE_MAXSIZE:-768}" "$FORTUNE_PATH"
     printf '\n'
 ) ; fi
