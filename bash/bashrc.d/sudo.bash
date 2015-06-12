07ed5d36387bcec7adb07af862c99a2316e3e438
Tom Ryder <tom@sanctum.geek.nz>
2015-06-12 14:29:30 +1200

    Remove sudo(8)/sudoedit(8) completion
    This was a bad idea. Completes commands for every argument, and will
    complete with users for the command to be run by sudo(8), not just
    sudo(8) itself. I'd have to tell the function too much about sudo(8)'s
    grammar to make this reliable.

--- bash/bashrc.d/sudo.bash
+++ bash/bashrc.d/sudo.bash
@@ -7,29 +7,3 @@ sudo() {
     fi
 }
 
-# Some imperfect but mostly-useful sudo(8) completion
-_sudo() {
-    word=${COMP_WORDS[COMP_CWORD]}
-    prev=${COMP_WORDS[COMP_CWORD-1]}
-
-    # Completion for this word depends on the previous word
-    case $prev in
-
-        # If the previous word was an option for -g, complete with group names
-        -*g)
-            COMPREPLY=( $(compgen -A group -- "$word") )
-            ;;
-
-        # If the previous word was an option for -u, complete with user names
-        -*u)
-            COMPREPLY=( $(compgen -A user -- "$word") )
-            ;;
-
-        # Otherwise complete with commands
-        *)
-            COMPREPLY=( $(compgen -A command -- "$word") )
-            ;;
-    esac
-}
-complete -F _sudo -o default sudo
-
