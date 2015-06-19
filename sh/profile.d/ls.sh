9a5d6d9f9d5361187e904f937af3cabd1e1071ca
Tom Ryder <tom@sanctum.geek.nz>
2015-06-19 14:19:08 +1200

    Don't bother checking for ls --color

--- sh/profile.d/ls.sh
+++ sh/profile.d/ls.sh
@@ -1,19 +1,8 @@
-# Store ls(1)'s --help output in a variable
-lshelp=$(ls --help 2>/dev/null)
-
-# Define and store appropriate colors for ls(1) if applicable
-case $lshelp in
-    *--color*)
-        if command -v dircolors >/dev/null 2>&1 ; then
-            if [ -r "$HOME"/.dircolors ] ; then
-                eval "$(dircolors --sh -- "$HOME"/.dircolors)"
-            else
-                eval "$(dircolors --sh)"
-            fi
-        fi
-        ;;
-esac
-
-# We're done parsing ls(1)'s --help output now
-unset -v lshelp
+if command -v dircolors >/dev/null 2>&1 ; then
+    if [ -r "$HOME"/.dircolors ] ; then
+        eval "$(dircolors --sh -- "$HOME"/.dircolors)"
+    else
+        eval "$(dircolors --sh)"
+    fi
+fi
 
