037f0abd361b7f83f52ff58901b98e1c0fbabe62
Tom Ryder <tom@sanctum.geek.nz>
2015-08-05 12:56:29 +1200

    Strip unneeded quotes

--- bash/bashrc.d/bd.bash
+++ bash/bashrc.d/bd.bash
@@ -4,7 +4,7 @@ bd() {
 
     # If there are no arguments, we just move up one directory (cd ..)
     if (($#)) ; then
-        dir="${PWD%/$1*}"/"$1"
+        dir=${PWD%/$1*}/$1
     else
         dir=..
     fi
