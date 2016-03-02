a2ff6f4e478689710a1bf3fdb8db97293a5fb6ec
Tom Ryder <tom@sanctum.geek.nz>
2016-03-03 10:03:43 +1300

    Tell shellcheck it's wrong about this line

--- bash/bashrc.d/scp.bash
+++ bash/bashrc.d/scp.bash
@@ -1,8 +1,7 @@
 # Wrap scp(1) to check for missing colons
 scp() {
-    local argstring
-    argstring=$*
-    if (($# >= 2)) && [[ $argstring != *:* ]] ; then
+    # shellcheck disable=SC2048
+    if (($# >= 2)) && [[ $* != *:* ]] ; then
         printf 'bash: %s: Missing colon, probably an error\n' \
             "$FUNCNAME" >&2
         return 2
