5b8b515c57e682fe9a41e8436be3755e54f17253
Tom Ryder <tom@sanctum.geek.nz>
2016-06-15 10:28:02 +1200

    Correct td(1) completion
    Forgot to narrow completions down to ones that match the current word

--- bash/bashrc.d/td.bash
+++ bash/bashrc.d/td.bash
@@ -7,10 +7,10 @@ _td() {
     done < <(
         shopt -s extglob nullglob
         shopt -u dotglob
-        declare -a fns
-        cd -- "$dir" || exit
-        fns=(*)
-        ((${#fns[@]})) || exit
+        local -a fns
+        fns=("$dir"/"${COMP_WORDS[COMP_CWORD]}"*)
+        fns=("${fns[@]#$dir/}")
+        ((${#fns[@]})) || exit 1
         printf '%s\0' "${fns[@]##$dir/}"
     )
     return
