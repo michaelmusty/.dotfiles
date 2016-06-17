1ddd1bd6f00c400ebb24d726f95302d767c8fd4e
Tom Ryder <tom@sanctum.geek.nz>
2016-06-17 12:58:22 +1200

    Correct a comment

--- sh/profile.d/grep.sh
+++ sh/profile.d/grep.sh
@@ -1,7 +1,7 @@
 # Store grep(1)'s --help output in a variable
 grep_help=$(grep --help 2>/dev/null)
 
-# Run dircolors(1) to export LS_COLORS if available and appropriate
+# Set and export GREP_COLORS if available and appropriate
 case $grep_help in
     *--color*)
         GREP_COLORS='ms=01;31:mc=01;31:sl=:cx=:fn=35:ln=32:bn=32:se=36'
