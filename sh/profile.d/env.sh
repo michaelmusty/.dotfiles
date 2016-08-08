f870a25ed27b1313e023e634e9ccf27b861b3631
Tom Ryder <tom@sanctum.geek.nz>
2016-08-09 11:14:52 +1200

    Add some notes about ENV for Bash

--- sh/profile.d/env.sh
+++ sh/profile.d/env.sh
@@ -1,4 +1,7 @@
-# If we're running some kind of ksh, export ENV to find ~/.kshrc
+# If we're running some kind of ksh, export ENV to find ~/.kshrc. Bash differs
+# considerably from this behaviour; it uses ENV as its startup file when it's
+# invoked as sh(1), and uses .bashrc or --rcfile as its interactive startup
+# file, so it doesn't need to be specified here.
 if [ -n "$KSH_VERSION" ] ; then
     ENV=$HOME/.kshrc
     export ENV
