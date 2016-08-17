1d5845eed2d9450c84ca046749c73ebde2bdfafb
Tom Ryder <tom@sanctum.geek.nz>
2016-08-17 20:15:17 +1200

    Explain ksh.sh a bit more

--- sh/shrc.d/ksh.sh
+++ sh/shrc.d/ksh.sh
@@ -1,5 +1,7 @@
 # If we're running some kind of ksh, we'll need to source its specific
-# configuration if it was defined or if we can find it
+# configuration if it was defined or if we can find it. Bash and Zsh invoke
+# their own rc files first, which I've written to then look for ~/.shrc; ksh
+# does it the other way around.
 case $KSH_VERSION in
     *'PD KSH '*)
         [ -f "${KSH_ENV:-"$HOME"/.pdkshrc}" ] || return
