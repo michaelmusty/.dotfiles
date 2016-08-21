1c792276fafe1f1c8a0d7ebbf103fabfc9a1e132
Tom Ryder <tom@sanctum.geek.nz>
2016-08-21 22:10:08 +1200

    Distill/fix keychain handling a bit

--- sh/profile.d/keychain.sh
+++ sh/profile.d/keychain.sh
@@ -1,17 +1,12 @@
 # ssh-askpass setup
-if command -v ssh-askpass >/dev/null 2>&1 ; then
-    SSH_ASKPASS=$(command -v ssh-askpass)
+[ -n "${SSH_ASKPASS:="$(command -v ssh-askpass 2>&1)"}" ] &&
     export SSH_ASKPASS
-fi
 
 # keychain setup
 if command -v keychain >/dev/null 2>&1 ; then
-    eval "$(TERM=${TERM:-ansi} keychain \
-        --eval --ignore-missing --quiet id_dsa id_rsa id_ecsda)"
 
-    # Set and export TTY/GPG_TTY for interactive shells
-    if [ -t 0 ] ; then
-        GPG_TTY=$(tty)
-        export GPG_TTY
-    fi
+    # Run keychain as quickly and quietly as possible
+    eval "$(TERM=${TERM:-ansi} keychain \
+        --eval --ignore-missing --quick --quiet \
+        id_dsa id_rsa id_ecsda)"
 fi
