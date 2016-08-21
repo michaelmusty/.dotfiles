075c3fb2b3f2d75886e9cffeb13d3d9353978fa3
Tom Ryder <tom@sanctum.geek.nz>
2016-08-21 23:16:02 +1200

    Stop versioning go.sh
    Pretty simple script, and only in active use on one of my systems at the
    moment anyway; just cluttering environment otherwise

--- sh/profile.d/go.sh
+++
@@ -1,6 +0,0 @@
-# Define path for Go code to be installed
-GOPATH=$HOME/.local/gocode
-export GOPATH
-
-# Prepend GOPATH to PATH for the executables if it exists
-[ -d "$GOPATH"/bin ] && PATH=$GOPATH/bin:$PATH
