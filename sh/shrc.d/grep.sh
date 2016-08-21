c2f05c595dfe71a60f48e14635af191c07bf6980
Tom Ryder <tom@sanctum.geek.nz>
2016-08-21 13:44:21 +1200

    Adjust ordering of ls() and grep() options

--- sh/shrc.d/grep.sh
+++ sh/shrc.d/grep.sh
@@ -5,14 +5,22 @@
 # Define function proper
 grep() {
 
-    # Add --exclude-dir to ignore version control dot-directories
-    [ -e "$HOME"/.cache/grep/exclude-dir ] &&
-        set -- \
-            --exclude-dir=.cvs \
-            --exclude-dir=.git \
-            --exclude-dir=.hg  \
-            --exclude-dir=.svn \
-            "$@"
+    # Add --binary-files=without-match to gracefully skip binary files
+    [ -e "$HOME"/.cache/grep/binary-files ] &&
+        set -- --binary-files=without-match "$@"
+
+    # Add --color if the terminal has at least 8 colors
+    [ -e "$HOME"/.cache/grep/color ] &&
+    [ "$({ tput colors || tput Co ; } 2>/dev/null)" -ge 8 ] &&
+        set -- --color=auto "$@"
+
+    # Add --devices=skip to gracefully skip devices
+    [ -e "$HOME"/.cache/grep/devices ] &&
+        set -- --devices=skip "$@"
+
+    # Add --directories=skip to gracefully skip directories
+    [ -e "$HOME"/.cache/grep/directories ] &&
+        set -- --directories=skip "$@"
 
     # Add --exclude to ignore .gitignore and .gitmodules files
     [ -e "$HOME"/.cache/grep/exclude ] &&
@@ -21,22 +29,14 @@ grep() {
             --exclude=.gitmodules \
             "$@"
 
-    # Add --directories=skip to gracefully skip directories
-    [ -e "$HOME"/.cache/grep/directories ] &&
-        set -- --directories=skip "$@"
-
-    # Add --devices=skip to gracefully skip devices
-    [ -e "$HOME"/.cache/grep/devices ] &&
-        set -- --devices=skip "$@"
-
-    # Add --color if the terminal has at least 8 colors
-    [ -e "$HOME"/.cache/grep/color ] &&
-    [ "$({ tput colors || tput Co ; } 2>/dev/null)" -ge 8 ] &&
-        set -- --color=auto "$@"
-
-    # Add --binary-files=without-match to gracefully skip binary files
-    [ -e "$HOME"/.cache/grep/binary-files ] &&
-        set -- --binary-files=without-match "$@"
+    # Add --exclude-dir to ignore version control dot-directories
+    [ -e "$HOME"/.cache/grep/exclude-dir ] &&
+        set -- \
+            --exclude-dir=.cvs \
+            --exclude-dir=.git \
+            --exclude-dir=.hg  \
+            --exclude-dir=.svn \
+            "$@"
 
     # Run grep(1) with the concluded arguments
     command grep "$@"
