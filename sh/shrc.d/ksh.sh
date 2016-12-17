11b4fc709805b3e3f106e0b39d55b4ea2e608afc
Tom Ryder <tom@sanctum.geek.nz>
2016-12-17 19:30:40 +1300

    Add a second version check to detect ksh

--- sh/shrc.d/ksh.sh
+++ sh/shrc.d/ksh.sh
@@ -2,7 +2,7 @@
 # configuration if it was defined or if we can find it. Bash and Zsh invoke
 # their own rc files first, which I've written to then look for ~/.shrc; ksh
 # does it the other way around.
-[ -n "$KSH_VERSION" ] || return
+[ -n "$KSH_VERSION" ] || [ -n "${.sh.version}" ] || return
 [ -n "$KSH_ENV" ] || KSH_ENV=$HOME/.kshrc
 [ -f "$KSH_ENV" ] || return
 . "$KSH_ENV"
