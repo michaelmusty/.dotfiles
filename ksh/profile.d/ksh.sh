09bc29e2dbeadc811491f555e71397dfa0264704
Tom Ryder <tom@sanctum.geek.nz>
2016-12-18 12:54:48 +1300

    Change tack; force ENV if the file exists

--- ksh/profile.d/ksh.sh
+++
@@ -1,6 +0,0 @@
-# If ksh93 as a login shell decided to give ENV a bizarre value, quietly shunt
-# it out of the way. This is probably not how I'm supposed to do this, but I
-# can't find documentation as to why ksh93 chooses this value.
-case $ENV in
-    .sh.ENV) [ -f "$ENV" ] || unset ENV ;;
-esac
