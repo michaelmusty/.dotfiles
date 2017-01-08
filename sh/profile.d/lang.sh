4dca4a2620fed992a0c71834f6ac6d7a72192741
Tom Ryder <tom@sanctum.geek.nz>
2017-01-09 00:50:17 +1300

    Much simpler LC_COLLATE approach

--- sh/profile.d/lang.sh
+++ sh/profile.d/lang.sh
@@ -1,15 +1,4 @@
-# Use the system's locale and language, but if it's not C or C.UTF-8, then
-# force LC_COLLATE to an appropriate C locale so that the order of sort and
-# glob expansion stays sane without making e.g. dates insane. Don't interfere
-# at all if LANG isn't even set.
-case $LANG in
-    C|C.UTF-8) ;;
-    *)
-        if locale -a | grep -q C.UTF-8 ; then
-            LC_COLLATE=C.UTF-8
-        else
-            LC_COLLATE=C
-        fi
-        export LC_COLLATE
-        ;;
-esac
+# Always use bytewise sorting if not already set
+[ -z "$LC_COLLATE" ] || return
+LC_COLLATE=C
+export LC_COLLATE
