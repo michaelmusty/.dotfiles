b7e47b0941ebd056f75bbf2d4a2e09a672b24170
Tom Ryder <tom@sanctum.geek.nz>
2017-05-02 00:26:25 +1200

    Clean up clog(1df) a bit

--- bin/clog.sh
+++ bin/clog.sh
@@ -1,7 +1,16 @@
+#!/bin/sh
 # Record a timestamped message to a logfile, defaulting to ~/.clog
 self=clog
+
+# Ignore arguments
+set --
+
+# If we have rlwrap, quietly use it
 command -v rlwrap >/dev/null 2>&1 &&
     set -- rlwrap -C "$self" "$@"
+
+# Write the date, the standard input (rlwrapped if applicable), and two dashes
+# to $CLOG, defaulting to ~/.clog.
 {
     date
     "$@" cat -
