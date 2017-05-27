8571498f0f8c27b35a2ed813ace1b0c04e2c1dfd
Tom Ryder <tom@sanctum.geek.nz>
2017-05-27 20:01:34 +1200

    Add history filename squashing to rlwrap(1) calls

--- bin/clog.sh
+++ bin/clog.sh
@@ -7,7 +7,7 @@ set --
 
 # If we have rlwrap, quietly use it
 command -v rlwrap >/dev/null 2>&1 &&
-    set -- rlwrap -C "$self" "$@"
+    set -- rlwrap --history-filename=/dev/null -C "$self" "$@"
 
 # Write the date, the standard input (rlwrapped if applicable), and two dashes
 # to $CLOG, defaulting to ~/.clog.
