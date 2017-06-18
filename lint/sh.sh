f372b831ed58b0c1abf41e90ac2560508e849960
Tom Ryder <tom@sanctum.geek.nz>
2017-06-18 16:35:55 +1200

    Update check/lint sh

--- lint/sh.sh
+++ lint/sh.sh
@@ -1,2 +1,6 @@
-find sh ksh/shrc.d mpd/profile.d \
+find sh \
+    keychain/profile.d keychain/shrc.d \
+    ksh/shrc.d \
+    mpd/profile.d \
+    plenv/profile.d plenv/shrc.d \
     -type f -print -exec shellcheck -e SC1090 -s sh -- {} +
