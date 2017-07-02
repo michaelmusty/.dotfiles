fcb11d71d7d4dd7abf5af3e58106154ab2dd2212
Tom Ryder <tom@sanctum.geek.nz>
2017-07-02 23:09:08 +1200

    Correct some rndi(1df) references

--- bin/mktd.sh
+++ bin/mktd.sh
@@ -5,7 +5,7 @@ seed=$(rnds)
 
 # Build the intended directory name, with the last element a random integer
 # from 1..2^31
-dn=${TMPDIR:-/tmp}/${1:-mktd}.$$.$(rndi 1 2147483648 "$seed")
+dn=${TMPDIR:-/tmp}/${1:-mktd}.$$.$(rndi 1 2147483648)
 
 # Create the directory and print its name if successful
 mkdir -m 700 -- "$dn" && printf '%s\n' "$dn"
