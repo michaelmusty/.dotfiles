694e3e4f6de5b6759c0bca0a50f653844e92c821
Tom Ryder <tom@sanctum.geek.nz>
2017-07-03 14:33:44 +1200

    Remove unused var from mktd(1df)

--- bin/mktd.sh
+++ bin/mktd.sh
@@ -1,8 +1,5 @@
 # Try to make a random temp directory
 
-# Get a random seed from rnds(1df); if it's empty, that's still workable
-seed=$(rnds)
-
 # Build the intended directory name, with the last element a random integer
 # from 1..2^31
 dn=${TMPDIR:-/tmp}/${1:-mktd}.$$.$(rndi 1 2147483648)
