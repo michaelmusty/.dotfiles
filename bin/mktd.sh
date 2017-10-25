2550842ce1e0ce8163fbf1956482292c01a6bbe4
Tom Ryder <tom@sanctum.geek.nz>
2017-10-26 09:07:45 +1300

    Make rndi(1df) upper bound a little clearer

--- bin/mktd.sh
+++ bin/mktd.sh
@@ -2,7 +2,8 @@
 
 # Build the intended directory name, with the last element a random integer
 # from 1..2^31
-dn=${TMPDIR:-/tmp}/${1:-mktd}.$$.$(rndi 1 2147483648)
+lim=$((2 << 31))
+dn=${TMPDIR:-/tmp}/${1:-mktd}.$$.$(rndi 1 "$e")
 
 # Create the directory and print its name if successful
 mkdir -m 700 -- "$dn" && printf '%s\n' "$dn"
