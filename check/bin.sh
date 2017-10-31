1601f3e9b40b8d13b3094ec324e0e0cd625994c9
Tom Ryder <tom@sanctum.geek.nz>
2017-10-31 22:31:31 +1300

    Use shell name not command in check/lint messages
    This is a little bit clearer and nicer to read, I think.

--- check/bin.sh
+++ check/bin.sh
@@ -2,14 +2,14 @@
 for bin in bin/*.sh ; do
     sh -n -- "${bin%.sh}" || exit
 done
-printf 'sh(1) binscripts parsed successfully.\n'
+printf 'POSIX sh binscripts parsed successfully.\n'
 
 # GNU Bash
 if command -v bash >/dev/null 2>&1 ; then
     for bin in bin/*.bash ; do
         bash -n -- "${bin%.bash}" || exit
     done
-    printf 'bash(1) binscripts parsed successfully.\n'
+    printf 'GNU Bash binscripts parsed successfully.\n'
 else
-    printf 'bash(1) not found, skipping checks.\n'
+    printf 'bash(1) not found, skipping GNU Bash checks.\n'
 fi
--- lint/bin.sh
+++ lint/bin.sh
@@ -4,7 +4,7 @@ for bin in bin/*.sh ; do
     set "$@" "${bin%.sh}"
 done
 shellcheck -e SC1090 -- "$@" || exit
-printf 'sh(1) binscripts linted successfully.\n'
+printf 'POSIX sh binscripts linted successfully.\n'
 
 # GNU Bash
 if command -v bash >/dev/null 2>&1 ; then
@@ -13,7 +13,7 @@ if command -v bash >/dev/null 2>&1 ; then
         set "$@" "${bin%.bash}"
     done
     shellcheck -e SC1090 -- "$@" || exit
-    printf 'bash(1) binscripts linted successfully.\n'
+    printf 'GNU Bash binscripts linted successfully.\n'
 else
-    printf 'bash(1) not found, skipping lint.\n'
+    printf 'bash(1) not found, skipping GNU Bash lint.\n'
 fi
