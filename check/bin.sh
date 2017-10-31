4cf6ed691373bd3cd0c84369e48f735db8d8fd30
Tom Ryder <tom@sanctum.geek.nz>
2017-10-31 22:05:59 +1300

    Add conditional Bash `check-bin`, `lint-bin`
    Both blocks are analogues of the POSIX checks, but are wrapped in a
    conditional so that bash(1) doesn't become a hard dependency of the
    default `make install` target.

--- check/bin.sh
+++ check/bin.sh
@@ -1,4 +1,15 @@
+# POSIX sh
 for bin in bin/*.sh ; do
     sh -n -- "${bin%.sh}" || exit
 done
 printf 'sh(1) binscripts parsed successfully.\n'
+
+# GNU Bash
+if command -v bash >/dev/null 2>&1 ; then
+    for bin in bin/*.bash ; do
+        bash -n -- "${bin%.bash}" || exit
+    done
+    printf 'bash(1) binscripts parsed successfully.\n'
+else
+    printf 'bash(1) not found, skipping checks.\n'
+fi
--- lint/bin.sh
+++ lint/bin.sh
@@ -1,6 +1,19 @@
+# POSIX sh
 set --
 for sh in bin/*.sh ; do
     set "$@" "${sh%.sh}"
 done
 shellcheck -e SC1090 -- "$@" || exit
 printf 'sh(1) binscripts linted successfully.\n'
+
+# GNU Bash
+if command -v bash >/dev/null 2>&1 ; then
+    set --
+    for bin in bin/*.bash ; do
+        set "$@" "${sh%.sh}"
+    done
+    shellcheck -e SC1090 -- "$@" || exit
+    printf 'bash(1) binscripts linted successfully.\n'
+else
+    printf 'bash(1) not found, skipping lint.\n'
+fi
