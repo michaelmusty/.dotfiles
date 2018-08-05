658341783f6562cc44e0fde7f2966d9505575a45
Tom Ryder <tom@sanctum.geek.nz>
2018-08-06 09:04:36 +1200

    Add explicit LC_COLLATE settings to plmu(1df)
    This prevents it getting upset if the sorting for the ignore list used a
    different collation.

--- bin/plmu.sh
+++ bin/plmu.sh
@@ -12,10 +12,10 @@ fi
 
 # Get the list of modules; sort them in case our current locale disagrees on
 # the existing sorting
-plenv list-modules | sort |
+plenv list-modules | LC_COLLATE=C sort |
 
 # Exclude any modules in ~/.plenv/non-cpanm-modules if it exists
-comm -23 -- - "$ef" |
+LC_COLLATE=C comm -23 -- - "$ef" |
 
 # Read that list of modules to upgrade and upgrade them one by one
 while read -r module ; do
