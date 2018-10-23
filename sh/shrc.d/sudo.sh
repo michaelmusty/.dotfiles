7454ee011907e0191ca42e63256135607a96f473
Tom Ryder <tom@sanctum.geek.nz>
2018-10-24 11:15:48 +1300

    Use case statement for sudo -v switching
    This is slightly more idiomatic shell.

--- sh/shrc.d/sudo.sh
+++ sh/shrc.d/sudo.sh
@@ -1,5 +1,8 @@
 # Add the -H parameter to sudo(8) calls, always use the target user's $HOME
 sudo() {
-    [ "$1" != -v ] && set -- -H "$@"
+    case $1 in
+        -v) ;;
+        *) set -- -H "$@" ;;
+    esac
     command sudo "$@"
 }
