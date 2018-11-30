9af93d72284d22b24e27080e38231f32c2e45f54
Tom Ryder <tom@sanctum.geek.nz>
2018-12-01 02:44:17 +1300

    Restore correct completion function for mutt(1)
    This was mistakenly changed to _text_filenames in 65e47bf.

--- bash/bash_completion.d/mutt.bash
+++ bash/bash_completion.d/mutt.bash
@@ -1,5 +1,5 @@
 # Completion for mutt(1) with abook(1) email addresses
-if ! declare -F _text_filenames >/dev/null ; then
-    source "$HOME"/.bash_completion.d/_text_filenames.bash
+if ! declare -F _abook_addresses >/dev/null ; then
+    source "$HOME"/.bash_completion.d/_abook_addresses.bash
 fi
 complete -F _abook_addresses -o bashdefault -o default mutt
