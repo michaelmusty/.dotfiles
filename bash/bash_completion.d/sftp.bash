d7f8465d51306c2e1539117e0d7f1dc066bbe1e3
Tom Ryder <tom@sanctum.geek.nz>
2018-12-01 02:04:41 +1300

    Use full `if` condition for SSH hostname comp src

--- bash/bash_completion.d/sftp.bash
+++ bash/bash_completion.d/sftp.bash
@@ -1,4 +1,5 @@
 # Completion for sftp(1) with ssh_config(5) hostnames
-declare -F _ssh_config_hosts >/dev/null ||
+if ! declare -F _ssh_config_hosts >/dev/null ; then
     source "$HOME"/.bash_completion.d/_ssh_config_hosts.bash
+fi
 complete -F _ssh_config_hosts -o bashdefault -o default sftp
