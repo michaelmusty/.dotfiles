7828d0f0ff2fc0fb3e2950b7093b7d6d7e62fb85
Tom Ryder <tom@sanctum.geek.nz>
2018-12-17 15:13:41 +1300

    Adjust PS1-PS4 reset

--- sh/shrc.d/prompt.sh
+++ sh/shrc.d/prompt.sh
@@ -1,8 +1,15 @@
-# Some systems' /etc/profile setups export PS1, which really fouls things up
-# when switching between non-login shells; let's put things right by unsetting
-# it to break the export and then just setting them as simple variables
-unset PS1 PS2 PS3 PS4
-PS1='$ ' PS2='> ' PS3='? ' PS4='+ '
+# Some systems' /etc/profile setups export their prompt strings (PS1, PS2...),
+# which really fouls things up when switching between non-login shells; let's
+# put things right by unsetting each of them to break the export, and then just
+# setting them as simple variables
+unset PS1
+PS1='$ '
+unset PS2
+PS2='> '
+unset PS3
+PS3='? '
+unset PS4
+PS4='+ '
 
 # If we have an SSH_CLIENT or SSH_CONNECTION environment variable, put the
 # hostname in PS1 too.
