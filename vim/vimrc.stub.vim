michaelmusty/.dotfiles

commit 5d67a92b1ee7d53aabe46e6fd4a56c40d855d9eb

Make .vimrc stub more resilient to older Vims

vim/vimrc.stub.vim
@@ -1,14 +1,14 @@
-" If we have Vim version >=7, and (implicitly) +eval, source real vimrc
+" If we have non-tiny Vim version >=7, source real vimrc; this works because
+" tiny and/or ancient builds of Vim quietly igore all code in :if blocks
 if v:version >= 700
   runtime vimrc
-
-" If not, prevent Vim from using any part of our configuration
-else
-  if has('win32') || has('win64')
-    set runtimepath-=~/vimfiles
-    set runtimepath-=~/vimfiles/after
-  else
-    set runtimepath-=~/.vim
-    set runtimepath-=~/.vim/after
-  endif
+  finish
 endif
+
+" Otherwise, prevent Vim from using any part of our configuration
+set runtimepath-=~/.vim
+set runtimepath-=~/.vim/after
+if has('win32') || has('win64')
+  set runtimepath-=~/vimfiles
+  set runtimepath-=~/vimfiles/after
+else

commit ac373a7c4ca875c3fa19b93606be1b7be823a69c

Correct error in stub .vimrc
This was introduced in commit 5d67a92. I'm surprised I haven't noticed
already. I would have thought this would cause glaring errors.

vim/vimrc.stub.vim
@@ -11,4 +11,4 @@ set runtimepath-=~/.vim/after
 if has('win32') || has('win64')
   set runtimepath-=~/vimfiles
   set runtimepath-=~/vimfiles/after
-else
+endif