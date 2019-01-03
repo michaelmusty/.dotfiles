97a0d713317036d773df6476f8ae47a1e423276f
Tom Ryder <tom@sanctum.geek.nz>
2019-01-04 10:18:06 +1300

    Further attempts at sane Emacs VISUAL setting

--- sh/profile.d/visual.sh
+++ sh/profile.d/visual.sh
@@ -1,24 +1,8 @@
-# If an Emacs is installed, and ~/.emacs exists, use emacs as the visual
-# editor; otherwise, use the system's vi
-if command -v emacs >/dev/null 2>&1 &&
-        [ -f "$HOME"/.emacs ] ; then
-
-    # Use or start a GNU Emacs client, if possible
-    if command -v pgrep >/dev/null 2>&1 &&
-            pgrep -fxu "$USER" 'emacs --daemon' >/dev/null ||
-            emacs --daemon 2>/dev/null ; then
-        VISUAL=emacsclient
-
-    # If no GNU Emacs daemon is available, just create a new instance every
-    # time
-    else
-        VISUAL=emacs
-    fi
-
-# If an Emacs isn't installed, just use good old vi
+# If my Emacs wrapper is installed, use emacs as the visual editor; otherwise,
+# use the system's vi
+if command -v emacsm >/dev/null 2>&1 ; then
+    VISUAL='emacsm'
 else
-    VISUAL=vi
+    VISUAL='vi'
 fi
-
-# Export final editor decision
 export VISUAL
