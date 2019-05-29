e7f5ef531ab3f8ec35b9b62e7c6ad823b5ea9148
Tom Ryder <tom@sanctum.geek.nz>
2019-05-29 18:11:45 +1200

    Add some nice comments to post-update Git hook

--- git/template/hooks/post-update.sh
+++ git/template/hooks/post-update.sh
@@ -1,4 +1,11 @@
+# Update server info automatically if this is a bare repository
+
+# Get whether this is bare from git-config(1); this returns the strings 'true'
+# or 'false'; I don't think you can coerce the exit value instead, which would
+# have been nice
 bare=$(git config core.bare)
+
+# If it's 'true', update the server info so that this works via cgit or Gitweb
 case $bare in
     true) git update-server-info ;;
 esac
